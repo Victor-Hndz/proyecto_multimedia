db42b499d1204f8c80a92c9d412512535df33bf0
Vic-Hndz-PC <victor.hernandez06@goumh.umh.es>
2023-05-19 12:12:00 +0200

    raton v2

--- assets/js/ratonBailarin.js
+++ assets/js/ratonBailarin.js
@@ -1,6 +1,16 @@
 var canvas = document.getElementById("ratonCanvas");
 var context = canvas.getContext("2d");
 
+// Dibujar los rectángulos rojos (salientes del cuerpo)
+context.fillStyle = "red";
+context.fillRect(150, 230, 20, 60);
+context.fillRect(230, 230, 20, 60);
+
+// Dibujar los rectángulos rojos (salientes del cuerpo)
+context.fillStyle = "red";
+context.fillRect(120, 270, 30, 20);
+context.fillRect(250, 270, 30, 20); 
+
 // Dibujar el círculo grande amarillo (cuerpo del ratón)
 context.beginPath();
 context.arc(200, 175, 80, 0, 2 * Math.PI);
@@ -29,11 +39,14 @@ context.fill();
 
 // Dibujar los rectángulos rojos (salientes del cuerpo)
 context.fillStyle = "red";
-context.fillRect(70, 130, 60, 20);
-context.fillRect(270, 130, 60, 20);
+context.save(); // Guardar el estado actual del lienzo
+context.translate(70, 130); // Mover el origen del lienzo al punto de inicio del primer rectángulo
+context.rotate((180 * Math.PI) / 180); // Rotar el lienzo en 90 grados en sentido horario
+context.fillRect(0, 0, 20, 60); // Dibujar el primer rectángulo en el origen del lienzo rotado
+context.restore(); // Restaurar el estado original del lienzo
 
-
-// Dibujar los rectángulos rojos (salientes del cuerpo)
-context.fillStyle = "red";
-context.fillRect(120, 380, 60, 20);
-context.fillRect(220, 380, 60, 20);
+context.save(); // Guardar el estado actual del lienzo
+context.translate(270, 130); // Mover el origen del lienzo al punto de inicio del segundo rectángulo
+context.rotate((90 * Math.PI) / 180); // Rotar el lienzo en 90 grados en sentido horario
+context.fillRect(0, 0, 20, 60); // Dibujar el segundo rectángulo en el origen del lienzo rotado
+context.restore(); // Restaurar el estado original del lienzo
