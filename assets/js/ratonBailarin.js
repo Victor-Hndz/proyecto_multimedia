24dbd2a947b24459af83488de081cb1ab7b65c2b
Vic-Hndz-PC <victor.hernandez06@goumh.umh.es>
2023-05-19 14:06:46 +0200

    ratón en proceso

--- assets/js/ratonBailarin.js
+++ assets/js/ratonBailarin.js
@@ -1,52 +1,204 @@
-var canvas = document.getElementById("ratonCanvas");
-var context = canvas.getContext("2d");
+const canvas = document.getElementById("ratonCanvas");
+const context = canvas.getContext("2d");
 
-// Dibujar los rectángulos rojos (salientes del cuerpo)
-context.fillStyle = "red";
-context.fillRect(150, 230, 20, 60);
-context.fillRect(230, 230, 20, 60);
+const pelo_color = "#db8239"; 
+const pelo_oscuro_color = "#ab5916";
+const camisa_color = "green";
+const pantalon_color = "#44b83e";
+const ojos_color = "white";
+const pupilas_color = "black";
+const orejas_color = "#fc868a";
+const borde = "black";
 
-// Dibujar los rectángulos rojos (salientes del cuerpo)
-context.fillStyle = "red";
-context.fillRect(120, 270, 30, 20);
-context.fillRect(250, 270, 30, 20); 
+const circulo = Math.PI * 2;
+const borde_size = 1;
 
-// Dibujar el círculo grande amarillo (cuerpo del ratón)
-context.beginPath();
-context.arc(200, 175, 80, 0, 2 * Math.PI);
-context.fillStyle = "yellow";
-context.fill();
+document.addEventListener("onload", estado_inicial());
 
-// Dibujar el triángulo mediano naranja (cuerpo del ratón)
-context.beginPath();
-context.moveTo(150, 50);
-context.lineTo(250, 50);
-context.lineTo(200, 150);
-context.closePath();
-context.fillStyle = "orange";
-context.fill();
+// Dibujar las piernas
+function piernas() {
+    var x_izq = 150;
+    var y_izq = 220;
+    var w_izq = 20;
+    var h_izq = 70;
 
-// Dibujar las orejas (dos círculos medianos rojos)
-context.beginPath();
-context.arc(150, 50, 30, 0, 2 * Math.PI);
-context.fillStyle = "red";
-context.fill();
+    var x_der = 230;
+    var y_der = y_izq;
+    var w_der = w_izq;
+    var h_der = h_izq;
 
-context.beginPath();
-context.arc(250, 50, 30, 0, 2 * Math.PI);
-context.fillStyle = "red";
-context.fill();
+    context.fillStyle = pantalon_color;
+    context.fillRect(x_izq, y_izq, w_izq, h_izq);
+    context.fillRect(x_der, y_der, w_der, h_der);
 
-// Dibujar los rectángulos rojos (salientes del cuerpo)
-context.fillStyle = "red";
-context.save(); // Guardar el estado actual del lienzo
-context.translate(70, 130); // Mover el origen del lienzo al punto de inicio del primer rectángulo
-context.rotate((180 * Math.PI) / 180); // Rotar el lienzo en 90 grados en sentido horario
-context.fillRect(0, 0, 20, 60); // Dibujar el primer rectángulo en el origen del lienzo rotado
-context.restore(); // Restaurar el estado original del lienzo
+    pies(x_izq, y_izq+h_izq, x_der, y_der+h_der);
+}
 
-context.save(); // Guardar el estado actual del lienzo
-context.translate(270, 130); // Mover el origen del lienzo al punto de inicio del segundo rectángulo
-context.rotate((90 * Math.PI) / 180); // Rotar el lienzo en 90 grados en sentido horario
-context.fillRect(0, 0, 20, 60); // Dibujar el segundo rectángulo en el origen del lienzo rotado
-context.restore(); // Restaurar el estado original del lienzo
+// Dibujar los pies
+function pies(x_izq, y_izq, x_der, y_der) {
+    var w_izq = 40;
+    var h_izq = 20;
+
+    var w_der = w_izq;
+    var h_der = h_izq;
+
+    var radio = 10;
+
+    context.fillStyle = pelo_oscuro_color
+    context.fillRect(x_izq, y_izq, w_izq, h_izq);
+    context.fillRect(x_der, y_der, w_der, h_der);
+     
+    //manos del ratón
+    context.fillStyle = pelo_oscuro_color;
+    context.beginPath(); // Comenzar el trazado de la forma
+    context.arc(x_izq+w_izq, y_izq+radio, radio, 0, circulo); // Dibujar el círculo en el primer brazo
+    context.arc(x_der+w_der, y_izq+radio, radio, 0, circulo); // Dibujar el círculo en el segundo brazo
+    context.closePath(); // Cerrar la forma
+    context.fill(); // Rellenar los círculos con el color
+}
+
+// Dibujar el cuerpo del ratón
+function cuerpo() {
+    context.strokeStyle = borde;
+    context.lineWidth = 1; // Ancho del borde
+    context.beginPath();
+    context.arc(200, 175, 80, 0, 2 * Math.PI);
+    context.fillStyle = camisa_color;
+    context.fill();
+    context.stroke();
+}
+
+// Dibujar la cabeza del ratón
+function cabeza() {
+    //Cabeza del ratón
+    context.strokeStyle = borde;
+    context.lineWidth = borde_size; // Ancho del borde
+    context.beginPath();
+    context.moveTo(150, 50);
+    context.lineTo(250, 50);
+    context.lineTo(200, 150);
+    context.closePath();
+    context.fillStyle = pelo_color;
+    context.fill();
+    context.stroke();
+
+    //Nariz del ratón
+    context.strokeStyle = borde;
+    context.lineWidth = borde_size; // Ancho del borde
+    context.beginPath();
+    context.arc(200, 150, 5, 0, 2 * Math.PI);
+    context.fillStyle = pelo_oscuro_color;
+    context.fill();
+    context.stroke();
+
+    //Ojos del ratón
+    context.strokeStyle = borde;
+    context.lineWidth = borde_size; // Ancho del borde
+    context.beginPath();
+    context.arc(185, 85, 8, 0, 2 * Math.PI);
+    context.fillStyle = ojos_color
+    context.fill();
+    context.stroke();
+
+    context.strokeStyle = borde;
+    context.lineWidth = borde_size; // Ancho del borde
+    context.beginPath();
+    context.arc(215, 85, 8, 0, 2 * Math.PI);
+    context.fillStyle = ojos_color
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.arc(185, 85, 3, 0, 2 * Math.PI);
+    context.fillStyle = pupilas_color;
+    context.fill();
+
+    context.beginPath();
+    context.arc(215, 85, 3, 0, 2 * Math.PI);
+    context.fillStyle = pupilas_color;
+    context.fill();
+}
+
+// Dibujar las orejas del ratón
+function orejas() {
+    context.strokeStyle = borde;
+    context.lineWidth = borde_size; // Ancho del borde
+    context.beginPath();
+    context.arc(150, 50, 30, 0, 2 * Math.PI);
+    context.fillStyle = pelo_color;
+    context.fill();
+    context.stroke();
+    
+    context.strokeStyle = borde;
+    context.lineWidth = borde_size; // Ancho del borde
+    context.beginPath();
+    context.arc(250, 50, 30, 0, 2 * Math.PI);
+    context.fillStyle = pelo_color;
+    context.fill();
+    context.stroke();
+
+    context.strokeStyle = borde;
+    context.lineWidth = borde_size; // Ancho del borde
+    context.beginPath();
+    context.arc(150, 50, 15, 0, 2 * Math.PI);
+    context.fillStyle = orejas_color;
+    context.fill();
+    context.stroke();
+
+    context.strokeStyle = borde;
+    context.lineWidth = borde_size; // Ancho del borde
+    context.beginPath();
+    context.arc(250, 50, 15, 0, 2 * Math.PI);
+    context.fillStyle = orejas_color;
+    context.fill();
+    context.stroke();
+}
+
+// Dibujar los brazos del ratón
+function brazos() {
+    var x_izq = 120;
+    var y_izq = 150;
+    var w_izq = 20;
+    var h_izq = 70;
+
+    var x_der = 260;
+    var y_der = y_izq;
+    var w_der = w_izq;
+    var h_der = h_izq;
+
+    var radio = 10;
+
+    //manos del ratón
+    context.fillStyle = pelo_oscuro_color;
+    context.beginPath(); // Comenzar el trazado de la forma
+    context.arc(x_izq+radio, radio*circulo+y_izq+radio, radio, 0, circulo); // Dibujar el círculo en el primer brazo
+    context.arc(x_der+radio, radio*circulo+y_izq+radio, radio, 0, circulo); // Dibujar el círculo en el segundo brazo
+    context.closePath(); // Cerrar la forma
+    context.fill(); // Rellenar los círculos con el color
+
+    context.strokeStyle = borde;
+    context.lineWidth = borde_size; // Ancho del borde
+    context.fillStyle = camisa_color;
+    context.beginPath(); // Comenzar el trazado de la forma
+    context.rect(x_izq, y_izq, w_izq, h_izq); // Dibujar el primer rectángulo
+    context.rect(x_der, y_der, w_der, h_der); // Dibujar el segundo rectángulo
+    context.closePath(); // Cerrar la forma
+    context.fill(); // Rellenar los brazos con el color
+    context.stroke(); // Dibujar el borde negro alrededor de los brazos
+
+    context.fillStyle = camisa_color;
+    context.beginPath(); // Comenzar el trazado de la forma
+    context.arc(x_izq+radio, y_izq, radio, 0, circulo); // Dibujar el círculo en el primer brazo
+    context.arc(x_der+radio, y_der, radio, 0, circulo); // Dibujar el círculo en el segundo brazo
+    context.closePath(); // Cerrar la forma
+    context.fill(); // Rellenar los círculos con el color
+}
+
+
+function estado_inicial() {
+    piernas();
+    cuerpo();
+    brazos();
+    cabeza();
+    orejas();
+}
