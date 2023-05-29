fe75da7526c33abca7dd6010e5ad5344b1d104a1
jordi <jordisegura2002@gmai.com>
2023-05-29 13:09:52 +0200

    comentado codigo del canvas

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,3 +1,4 @@
+//Añadimos un listener para atender al input de teclado
 document.addEventListener("keypress", function (event) {
     switch (event.key) {
         case "w":
@@ -17,9 +18,9 @@ document.addEventListener("keypress", function (event) {
             myGame.player.speed[0] = 1;
             break;
     }
-    myGame.player.speed = normalizeVector(myGame.player.speed);
+    myGame.player.speed = normalizeVector(myGame.player.speed);//normalizamos la velocidad
 });
-
+//Añadimos un listener para cuando se detiene el input de teclado para frenar al jugador
 document.addEventListener("keyup", function (event) {
     switch (event.key) {
         case "w":
@@ -39,9 +40,9 @@ document.addEventListener("keyup", function (event) {
             myGame.player.speed[0] = 0;
             break;
     }
-    myGame.player.speed = normalizeVector(myGame.player.speed);
+    myGame.player.speed = normalizeVector(myGame.player.speed);//normalizamos la velocidad
 });
-
+//clase que describe los peces del juego
 class Artifact {
     constructor(id, x, y, width, height) {
         this.id = id;
@@ -52,11 +53,11 @@ class Artifact {
         this.sprite = new Image();
         this.sprite.src = "assets/img/svg/pez.svg";
     }
-
+    //dibujamos a los peces en el canvas
     draw() {
         myGame.context.drawImage(this.sprite, this.x, this.y, this.width, this.height);
     }
-
+    //movemos a los peces
     move() {
         if (this.x < 0 - this.width) {
             this.x = myGame.canvas.clientWidth + this.width;
@@ -75,10 +76,11 @@ class Player {
         this.sprite = new Image();
         this.sprite.src = "assets/img/svg/rana.svg"
     }
-
+    //dibumos al jugador en el canvas
     draw() {
         myGame.context.drawImage(this.sprite, this.x, this.y, this.width, this.height);
     }
+    //movemos al jugador en el canvas en función de su velocidad
     move() {
         var tempX = this.x + this.speed[0] * 2;
         var tempY = this.y + this.speed[1] * 2;
@@ -101,7 +103,7 @@ class Player {
     }
 }
 
-
+//iniciamos el juego linkamos el canvas y creamos los peces
 var myGame = {
     start: function () {
         myGame.is_over = false;
@@ -116,6 +118,7 @@ var myGame = {
         this.artifacts.push(new Artifact(2, 400, 400, 25, 25));
         this.artifacts.push(new Artifact(3, 500, 500, 25, 25));
     },
+    //función para limpiar el canvas
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
     }
@@ -123,24 +126,24 @@ var myGame = {
 function startGame() {
     myGame.start();
 }
-
+//funcion que se ejecuta a modo de gameloop
 function updateGame() {
 
-    myGame.clear();
-    if (myGame.artifacts.length == 0 && myGame.is_over == false) {
+    myGame.clear();//Limpiamos
+    if (myGame.artifacts.length == 0 && myGame.is_over == false) {//comprobamos que no se haya acabado el juego
         tracks = tracks.concat(hiddenTrack);
         cargarCanciones();
         myGame.is_over = true;
     }
-    myGame.player.move();
-    myGame.player.draw();
+    myGame.player.move();//movmeos al jugador
+    myGame.player.draw();//pintamos al jugador
 
-    myGame.artifacts.forEach(artifact => {
+    myGame.artifacts.forEach(artifact => {//movemos a todos los peces y los dibujamos
         artifact.move();
         artifact.draw();
     });
 
-    myGame.artifacts.forEach(artifact => {
+    myGame.artifacts.forEach(artifact => {//si colisioanmos con un pez lo eliminamos y seleccionamos la canción
         if (artifact.x < myGame.player.x + myGame.player.width &&
             artifact.x + artifact.width > myGame.player.x &&
             artifact.y < myGame.player.y + myGame.player.height &&
